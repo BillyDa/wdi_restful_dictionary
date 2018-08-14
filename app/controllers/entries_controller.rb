5c0a68f088cb97249e83ab863ac9af8de16a1005
Billy Davidson <billy.davids52@gmail.com>
2018-08-14 16:09:43 -0400

    complete

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -27,14 +27,23 @@ class EntriesController < ApplicationController
   end
 
   def edit
+    @entry = Entry.find(params[:id])
 
   end
 
   def update
-    redirect_to entry_url(params[:id])
+    @entry = Entry.find(params[:id])
+    @entry.word = params[:entry][:word]
+    @entry.language = params[:entry][:language]
+    @entry.definition = params[:entry][:definition]
+    if entry.save
+      redirect_to entry_url(params[:id])
+    end
   end
 
   def destroy
+    @entry = Entry.find(params[:id])
+    @entry.destroy
     redirect_to entries_url
   end
 
